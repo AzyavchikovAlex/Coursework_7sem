bf13156cb07ce76d2c754059effebf72aa9d44d3
AzyavchikovAlex <azyavchikov.alex@gmail.com>
2024-12-23 21:56:44 +0300

    delete junk

--- cmake-build-debug/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alexazyavchikov/Documents/BSU/Coursework_7sem/cmake-build-debug/_deps/googletest-src/googletest
-# Build directory: /home/alexazyavchikov/Documents/BSU/Coursework_7sem/cmake-build-debug/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
